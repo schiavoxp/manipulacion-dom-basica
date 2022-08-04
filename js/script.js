8b7791cebed5a3bf90d8e7d7de9dbd4c77716d75
schiavoxp <andyc.schiavi@gmail.com>
2022-08-03 21:08:48 -0500

    Modificar el html con JS

--- js/script.js
+++ js/script.js
@@ -1,15 +1,21 @@
-// const parrafo_1 = document.getElementsByClassName("parrafito");
-// const parrafo_2 = document.getElementById("pid");
-// const inp = document.getElementsByTagName("input");
-
+const h1 = document.querySelector("h1");
+const p = document.querySelector("p");
 const parrafo_1 = document.querySelector(".parrafito");
 const parrafo_2 = document.querySelector("#pid");
 const inp = document.querySelector("input");
 
-console.log(inp.value);
+// Modifica texto o HTML con innerHTML
+h1.innerText = "Patito Feo";
+// Modifica o añade atributos
+h1.setAttribute("class", "patito")
 
-console.log({
-    parrafo_1,
-    parrafo_2,
-    inp
-})
+// Añade clase a la lista de clases
+h1.classList.add('rojo');
+h1.classList.remove('patito')
+
+// inp.value = 'Hey'
+
+const img = document.createElement('img');
+img.setAttribute('src', 'https://imgs.search.brave.com/3UMBkgOvoTk19oXOu-I-yVFXEKhXh-J1qOylEid0CQE/rs:fit:844:225:1/g:ce/aHR0cHM6Ly90c2Uy/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5E/QXp5R3Q5cGl4TnVx/aERad05hYzNBSGFF/SyZwaWQ9QXBp');
+
+parrafo_2.appendChild(img)
