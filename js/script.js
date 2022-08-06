cc41888966ea6f1345e4ab6f411e7e853f86c05b
schiavoxp <andyc.schiavi@gmail.com>
2022-08-05 22:03:51 -0500

    Event listeners avanzado kinda

--- js/script.js
+++ js/script.js
@@ -2,9 +2,13 @@ const btn = document.querySelector("#btnCalcular");
 const input1 = document.querySelector("#numero1");
 const input2 = document.querySelector("#numero2");
 const result = document.querySelector(".result");
-btn.addEventListener("click", btnClick);
+const form = document.querySelector(".form");
+// btn.addEventListener("click", btnClick);
 
-function btnClick () {
-    console.log("Escuchando el evento click!");
-    result.innerText = Number(input1.value) + Number(input2.value);
-}
+// form.addEventListener('submit', btnClick);
+
+// function btnClick (event) {
+//     event.preventDefault();
+//     console.log("Escuchando el evento click!");
+//     result.innerText = Number(input1.value) + Number(input2.value);
+// }
